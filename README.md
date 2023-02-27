73463f09495e466738d047e516091c2a97091a1d
Josia Orava <josia.orava@gmail.com>
2023-02-27 19:32:54 +0200

    README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Particle Simulation
+![](https://github.com/JosiaOrava/particleSimulation/img/readme.gif)
+This is hobby project done by me to learn more on making particle physics.
+<br> It is made completely on Python and currently has particles for stone and sand. Stone will remain in place where you put it and sand falls and spreads.
+
+## What I learned
+This project has teached me to work with Cellular Automation, OOP and Python library called PyGame.
+<br> I'm sure my way of doing things isn't the most optimal and once I have added few other particle materials I will work on optimizing.
+
+## Future features
+Next I am working on water element and after that fire and distruction.
+<br> After those I will work on optimizing this sense now it runs into problems when there are many particles because it runs every frame alot of calculations on each particle.
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 pg.init()
 WIN_WIDTH, WIN_HEIGHT = 700, 600
 WIN = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -6,7 +7,8 @@ pg.display.set_caption("Particle Simulation")
 FPS = 30
 SAND = (194, 178, 128)
 ROCK = (128,132,135)
-RECT_SIZE = 7
+WATER = (0, 255, 255)
+RECT_SIZE = 10
 
 particles = []
 class Particle:
@@ -18,6 +20,21 @@ class Particle:
         self.type = type
         self.rect = pg.Rect(self.x,self.y,RECT_SIZE,RECT_SIZE)
     
+    def randomSide(left,right):
+        side = random.randint(0,1)
+        if side == 0:
+            if left == False:
+                return 2
+            elif right == False:
+                return 3
+            return 0
+        else:
+            if right == False:
+                return 3
+            elif left == False:
+                return 2
+            return 0
+
     def checkSides(self, x, y):
         self.left, self.right = False, False
         for i in particles:
@@ -27,11 +44,7 @@ class Particle:
             # Check if right pixel under is empty
             elif i.y == y + 1 and i.x == x + 1:
                 self.right = True
-        if self.left:
-            if self.right == False:
-                return 3
-            return 0
-        return 2
+        return Particle.randomSide(self.left, self.right)
         
     def checkColl(self, x,y):
         for particle in particles:
@@ -39,21 +52,83 @@ class Particle:
             if particle.y == y + 1 and particle.x == x:
                 return Particle.checkSides(self,x,y)
         return 1
-      
+
+    def fall(self):
+        if Particle.checkColl(self,self.x, self.y) == 1 and self.y <= WIN_HEIGHT // RECT_SIZE - 2:
+            self.y = self.y + 1
+        if Particle.checkColl(self, self.x, self.y) == 2 and self.x >= 2:
+            self.x = self.x - 1 
+        if Particle.checkColl(self, self.x, self.y) == 3 and self.x <= WIN_WIDTH // RECT_SIZE - 3:
+            self.x = self.x + 1  
               
             
         
 
 class Sand(Particle):
+    pass
+    
+
+class Water(Particle):
+
+    
+    def randomSide(left,right,leftUnder,rightUnder):
+        side = random.randint(0,1)
+        if side == 0:
+            if left == False:
+                return 5
+            elif right == False:
+                return 4
+            elif leftUnder == False:
+                return 2
+            elif rightUnder == False:
+                return 3
+            return 0
+        else:
+            if left == False:
+                return 5
+            elif right == False:
+                return 4
+            elif rightUnder == False:
+                return 3
+            elif leftUnder == False:
+                return 2
+            return 0
+
+    def checkSides(self, x, y):
+        self.left, self.right, self.leftUnder, self.rightUnder = False, False, False, False
+        for i in particles:
+            # Check if left pixel under is empty
+            if i.y == y + 1 and i.x == x - 1:
+                self.leftUnder = True
+            # Check if right pixel under is empty
+            elif i.y == y + 1 and i.x == x + 1:
+                self.rightUnder = True
+            # Check if left pixel is empty 
+            elif i.y == y and i.x == x - 1:
+                print("left true")
+                self.left = True
+            # Check if right pixel is empty
+            elif i.y == y and i.x == x + 1:
+                print("right true")
+                self.right = True
+        return Water.randomSide(self.left, self.right, self.leftUnder, self.rightUnder)
 
     def fall(self):
-        if Sand.checkColl(self,self.x, self.y) == 1 and self.y <= WIN_HEIGHT // RECT_SIZE - 2:
-            print(self.y) 
+        if Water.checkColl(self, self.x, self.y) == 1 and self.y <= WIN_HEIGHT // RECT_SIZE - 2:
             self.y = self.y + 1
-        if Sand.checkColl(self, self.x, self.y) == 2 and self.x >= 2:
+        if Water.checkColl(self, self.x, self.y) == 2 and self.x >= 2:
             self.x = self.x - 1 
-        if Sand.checkColl(self, self.x, self.y) == 3 and self.x <= WIN_WIDTH // RECT_SIZE - 3:
+        if Water.checkColl(self, self.x, self.y) == 3 and self.x <= WIN_WIDTH // RECT_SIZE - 3:
             self.x = self.x + 1
+        if Water.checkColl(self, self.x, self.y) == 4 and self.x >= 2:
+            print("left")
+            self.x = self.x - 1
+        if Water.checkColl(self, self.x, self.y) == 5 and self.x <= WIN_WIDTH // RECT_SIZE - 3:
+            print("right")
+            self.x = self.x + 1   
+
+
+
 class Rock(Particle):
     pass
 
@@ -71,6 +146,8 @@ def main():
         mouseStatus = pg.mouse.get_pressed()
         if mouseStatus[0]:
             particles.append(Sand(pg.mouse.get_pos(), "sand"))
+        if mouseStatus[1]:
+            particles.append(Water(pg.mouse.get_pos(), "water"))
         if mouseStatus[2]:
             particles.append(Rock(pg.mouse.get_pos(), "rock"))
         for particle in particles:
@@ -79,6 +156,9 @@ def main():
                 particle.fall()
             if particle.type == "rock":
                 pg.draw.rect(WIN, ROCK, (particle.x*RECT_SIZE, particle.y*RECT_SIZE,RECT_SIZE,RECT_SIZE))
+            if particle.type == "water":
+                pg.draw.rect(WIN, WATER, (particle.x*RECT_SIZE, particle.y*RECT_SIZE,RECT_SIZE,RECT_SIZE))
+                particle.fall()
         
         pg.display.update()
     pg.quit()
